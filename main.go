9e252d85ed7b36a365e5c7fbe4157822fd459d43
vic <74506415+v1ctorio@users.noreply.github.com>
2024-06-25 12:19:30 +0200

    implement zipping

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"archive/zip"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"path/filepath"
+	"strconv"
+	"time"
 
 	"github.com/urfave/cli"
 )
@@ -31,6 +37,97 @@ type Dir struct {
 	Files   []File
 	ZipPath string
 	ZipName string
+}
+
+func getTempZipPath(parent string) string {
+	// get the temp directory to store the zip file
+	// create a random string to append to the parent directory
+	salt := strconv.FormatInt(time.Now().UnixMilli(), 10)
+	tempdir := filepath.Join(parent, salt+"boar.zip.tmp")
+	return tempdir
+}
+func rmtempzipdir(dir string) {
+
+	err := os.RemoveAll(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func zipFolder(folder string) string {
+	log.Println("Zipping ", folder)
+	// zip the directory
+	zipdir := getTempZipPath(folder)
+
+	zipFile, err := os.Create(zipdir)
+	if err != nil {
+		return err.Error()
+
+	}
+	defer zipFile.Close()
+
+	archive := zip.NewWriter(zipFile)
+	defer archive.Close()
+
+	err = filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
+		log.Println("Walking", path)
+
+		if err != nil {
+			log.Println("Error walking", path, ":", err)
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		fileHeader, err := zip.FileInfoHeader(info)
+		if err != nil {
+			log.Println("Error creating file header for", path, ":", err)
+			return err
+		}
+
+		// Set the name of the file within the zip archive
+		fileHeader.Name, err = filepath.Rel(folder, path)
+		if err != nil {
+			log.Println("Error getting relative path for", path, ":", err)
+			return err
+		}
+
+		// Open the file
+		file, err := os.Open(path)
+		if err != nil {
+			log.Println("Error opening file", path, ":", err)
+			return err
+		}
+		defer file.Close()
+
+		writer, err := archive.CreateHeader(fileHeader)
+		if err != nil {
+			log.Println("Error creating writer for", path, ":", err)
+			return err
+		}
+
+		// Copy the file's contents to the zip archive
+		_, err = io.Copy(writer, file)
+		if err != nil {
+			log.Println("Error copying file", path, ":", err)
+			return err
+		}
+
+		return nil
+	})
+
+	log.Println("Finished walking")
+
+	if err != nil {
+		fmt.Println("Error zipping directory:", err)
+		return err.Error()
+	}
+
+	fmt.Println("Directory zipped successfully.")
+	return zipdir
+
 }
 
 func run(c *cli.Context) error {
